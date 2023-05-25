33959ebcb1d8343a2ad02d9b5d8be3a9f6a93be4
SergioVR <sergiovargas042001@gmail.com>
2023-05-25 00:13:47 -0500

    EjemploSuperficie

--- Graphic1.py
+++ Graphic1.py
@@ -1,16 +1,23 @@
-import matplotlib.pyplot as plt
 import numpy as np
-import math
+import matplotlib.pyplot as plt
+from mpl_toolkits import mplot3d
 
-L = 100
-V = 10
-x = np.arange(0,L,0.1)
-y = x*np.cos(x)
+fig = plt.figure(figsize=(8,6))
+ax3d = plt.axes(projection="3d")
 
+xdata = np.linspace(-3,3,100)
+ydata = np.linspace(-3,3,100)
+X,Y = np.meshgrid(xdata,ydata)
+Z = 1/(1+np.exp(-X-Y))
 
+ax3d = plt.axes(projection='3d')
+ax3d.plot_surface(X, Y, Z,cmap='plasma')
+ax3d.set_title('Surface Plot in Matplotlib')
+ax3d.set_xlabel('X')
+ax3d.set_ylabel('Y')
+ax3d.set_zlabel('Z')
 
-plt.plot(x,y)
-plt.xlabel('x')
-plt.ylabel('y')
-plt.title('Lab DLS')
 plt.show()
+
+
+#https://www.delftstack.com/es/howto/matplotlib/how-to-create-a-surface-plot-in-matplotlib/
